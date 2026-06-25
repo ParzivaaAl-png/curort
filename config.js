e41745c11a9bf8428c61f0b6611b5048be6e24dd
ParzivaaAl-png <hajrihanserzod580@gmail.com>
2026-06-25 16:25:31 +0500

    Connect Firebase project

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyA_xFyZhzJOnRzi0HO2MYGoWgIINzdS89I",
+  authDomain: "lola-curort.firebaseapp.com",
+  projectId: "lola-curort",
+  storageBucket: "lola-curort.firebasestorage.app",
+  messagingSenderId: "879717395106",
+  appId: "1:879717395106:web:565dfb25940278a83985fb",
+  measurementId: "G-H4JSPGHPHS",
 };
 
-export const WHATSAPP_PHONE = "77000000000";
+export const WHATSAPP_PHONE = "77760034405";
 export const RESORT_NAME = "Lola Kurort";
